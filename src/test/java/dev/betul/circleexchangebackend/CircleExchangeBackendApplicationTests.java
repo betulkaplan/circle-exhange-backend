a82c787e3cf381380870672d2bed8a01e5c220d9
betulkaplan-amplo <99176340+betulkaplan-amplo@users.noreply.github.com>
2023-05-25 13:31:53 +0200

    initial commit

---
+++ src/test/java/dev/betul/circleexchangebackend/CircleExchangeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.betul.circleexchangebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleExchangeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
