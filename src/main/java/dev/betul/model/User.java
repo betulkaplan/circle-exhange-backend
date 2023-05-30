2975971ae7142c50871ae052fd4ad5dffe76657a
Betul Kaplan <betulkaplan7@gmail.com>
2023-05-30 12:20:32 +0200

    add lombok dependency and refactor User model class

--- src/main/java/dev/betul/model/User.java
+++ src/main/java/dev/betul/model/User.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 import java.util.UUID;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="users")
 public class User {
@@ -16,43 +24,5 @@ public class User {
     @GeneratedValue
     private UUID id;
     private String email;
-
-    public User(UUID id, String name, String email, Integer age) {
-        this.id = id;
-        this.email = email;
-    }
-
-    public User() {
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email);
-    }
 }
 
