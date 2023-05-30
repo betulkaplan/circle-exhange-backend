40bc8eb62ca04b377d52f2dd8676a4ff402bd591
Betul Kaplan <betulkaplan7@gmail.com>
2023-05-30 16:56:15 +0200

    validate JWT

--- src/main/java/dev/betul/repository/UserRepository.java
+++ src/main/java/dev/betul/repository/UserRepository.java
@@ -3,7 +3,9 @@ package dev.betul.repository;
 import dev.betul.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository  extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
 }
